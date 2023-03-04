7c62f6db5f78432dd88cb654ec64741854169e6a
Enzo Glauber <enzojob@gmail.com>
2023-03-03 23:30:51 -0300

    class 9: useContext #1

--- chapter-7/hooks/src/views/App.jsx
+++ chapter-7/hooks/src/views/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 import Content from '../components/layout/Content';
 import Menu from '../components/layout/Menu';
+import DataContext, { data } from '../data/DataContext';
 
 const App = () => {
+  const [state, setState] = useState(data);
+
   return (
-    <div className="App">
-      <Router>
-        <Menu />
-        <Content />
-      </Router>
-    </div>
+    <DataContext.Provider value={{ state, setState }}>
+      <div className="App">
+        <Router>
+          <Menu />
+          <Content />
+        </Router>
+      </div>
+    </DataContext.Provider>
   );
 };
 
--- chapter-7/hooks/src/views/examples/UseContext.jsx
+++ chapter-7/hooks/src/views/examples/UseContext.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import DataContext from '../../data/DataContext';
 
 const UseContext = () => {
+  const context = useContext(DataContext);
+  console.log(`context`, context);
+
+  function setNumber(delta) {
+    context.setState(state => ({ ...state, number: state.number + delta }));
+  }
+
   return (
     <div className="UseContext">
       <PageTitle title="Hook UseContext" subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!" />
+      <div className="center">
+        <span className="text">{context.state.text}</span>
+        <span className="text">{context.state.number}</span>
+        <div>
+          <button className="btn" onClick={() => setNumber(-1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setNumber(1)}>
+            +1
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
